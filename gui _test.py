268f34bdfd5224ebcf7aa7fa3b3974ec076df5db
Dosu333 <vladamir1865@gmail.com>
2023-07-11 08:12:02 +0100

    Add signal plotting

--- gui _test.py
+++ gui _test.py
@@ -1,19 +1,21 @@
+import sys
+import random
 import matplotlib.pyplot as plt
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QGridLayout, QVBoxLayout, QWidget, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QVBoxLayout, QWidget, QPushButton, QLabel
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from thread import CueGeneratorThread
 
-class SignalWindow(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self, iterations, trials, epoch_time):
-        super(SignalWindow, self).__init__()
+        super(MainWindow, self).__init__()
 
         self.iterations = iterations
         self.trials = trials
-        self.epoch_time = float(epoch_time)
+        self.epoch_time = epoch_time
 
         # Create a QLabel for the large section
-        self.large_label = QLabel("Look here for the cues")
+        self.large_label = QLabel("Large Section")
         self.large_label.setAlignment(Qt.AlignCenter)
 
         # Create FigureCanvases for the small plots
@@ -37,6 +39,9 @@ class SignalWindow(QMainWindow):
         central_widget = QWidget(self)
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+
+        # Call the function to plot the data
+        # self.plot_data()
 
     def start_clicked(self):
         # Create the cue generator thread and connect the output signal to the output label
@@ -67,21 +72,53 @@ class SignalWindow(QMainWindow):
 
         # Create subplots and plot the data
         ax1 = self.small_canvas1.figure.add_subplot(111)
-        ax1.scatter(readings, y, color='red', alpha=0.7)
-        ax1.set_facecolor('none')
-        ax1.set_title('Raw EMG Signal')
+        ax1.scatter(readings, y, color='red')
 
         ax2 = self.small_canvas2.figure.add_subplot(111)
-        ax2.scatter(readings, y, color='green', alpha=0.7)
-        ax2.set_facecolor('none')
-        ax2.set_title('Filtered EMG Signal')
+        ax2.scatter(readings, y, color='green')
 
         ax3 = self.small_canvas3.figure.add_subplot(111)
-        ax3.scatter(readings, y, color='blue', alpha=0.7)
-        ax3.set_facecolor('none')
-        ax3.set_title('Analysis')
+        ax3.scatter(readings, y, color='blue')
 
         # Refresh the canvases
         self.small_canvas1.draw()
         self.small_canvas2.draw()
         self.small_canvas3.draw()
+
+        
+    # def plot_data(self):
+    #     # Generate some example data
+    #     x1 = [1, 8, 9, 2, 4]
+    #     x2 = [9, 4, 1, 5, 3]
+    #     x3 = [2, 4, 7, 1, 5]
+
+    #     y1 = [2, 4, 6, 8, 10]
+    #     y2 = [1, 3, 5, 7, 9]
+    #     y3 = [3, 6, 9, 12, 15]
+
+    #     # Clear the previous plots
+    #     self.small_canvas1.figure.clear()
+    #     self.small_canvas2.figure.clear()
+    #     self.small_canvas3.figure.clear()
+
+    #     # Create subplots and plot the data
+    #     ax1 = self.small_canvas1.figure.add_subplot(111)
+    #     ax1.scatter(x1, y1, color='red')
+
+    #     ax2 = self.small_canvas2.figure.add_subplot(111)
+    #     ax2.scatter(x2, y2, color='green')
+
+    #     ax3 = self.small_canvas3.figure.add_subplot(111)
+    #     ax3.scatter(x3, y3, color='blue')
+
+    #     # Refresh the canvases
+    #     self.small_canvas1.draw()
+    #     self.small_canvas2.draw()
+    #     self.small_canvas3.draw()
+
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec_())
--- thread.py
+++ thread.py
@@ -1,11 +1,13 @@
 import time
 import numpy as np
 from PyQt5.QtCore import QThread, pyqtSignal
+from matplotlib.figure import Figure
 from gui_cue_generator import cue_generator
 from gui_emg_object import emg_signal
 
 class CueGeneratorThread(QThread):
     output_signal = pyqtSignal(str)
+    array = pyqtSignal(np.ndarray)
 
     def __init__(self, iterations, trials, epoch_time, file_name=' '):
         super().__init__()
@@ -13,8 +15,8 @@ class CueGeneratorThread(QThread):
         self.trials = int(trials)
         self.epoch_time = float(epoch_time)
         self.dataset = {}
-        data_size = int(self.epoch_time/0.001)
-        self.shape = np.empty((0, data_size))
+        self.data_size = int(self.epoch_time/0.001)
+        self.shape = np.empty((0, self.data_size))
         self.emg = emg_signal(26, self.epoch_time) 
 
         # if file_name != ' ':
@@ -26,25 +28,26 @@ class CueGeneratorThread(QThread):
     def run(self):
         DAQ = 0
         self.output_signal.emit('Start of Sessions')
-        time.sleep(2)
+        # time.sleep(2)
 
         while DAQ != self.iterations:
             DAQ += 1
             cue_class, cues, useTime = cue_generator(self.trials)
             self.dataset[cue_class] = {}
-            self.output_signal.emit(f'Cue Class- {cue_class}')
-            time.sleep(2)
 
             for cue in cues:   
+                # self.output_signal.emit(cue)
+                self.readings = self.emg.continuous_read() 
                 self.output_signal.emit(cue)
-                readings = self.emg.continuous_read()  
+                self.array.emit(self.readings) 
                 # time.sleep(self.epoch_time)
                 
                 try:
                     if not self.dataset[cue_class].get(cue, None):
-                        self.dataset[cue_class][cue] = readings
+                        self.dataset[cue_class][cue] = self.readings
                 except:
-                    self.dataset[cue_class][cue] = np.vstack((self.dataset[cue_class][cue], readings))
+                    self.dataset[cue_class][cue] = np.vstack((self.dataset[cue_class][cue], self.readings))
+            
                 print(cue)
                 print(self.dataset[cue_class][cue].shape)
         self.output_signal.emit('End of sessions')
