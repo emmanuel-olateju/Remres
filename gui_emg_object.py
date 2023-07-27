6b964203326f6bfdc6a200b9b1d2563443b6de47
Dosu333 <vladamir1865@gmail.com>
2023-07-27 17:56:58 +0100

    uart and pyserial

--- gui_emg_object.py
+++ gui_emg_object.py
@@ -1,28 +1,28 @@
-# import machine
-# import utime
+import machine
+import utime
 import time
 import random
 import numpy as np
 
-# emg_adc = machine.ADC(26)
+emg_adc = machine.ADC(26)
 conversion_factor = 3.3 / (65535)
 
 class emg_signal:
     def __init__(self,adc_pin,epoch_time,file_name=' '):
-        # self.__adc = machine.ADC(adc_pin)
+        self.__adc = machine.ADC(adc_pin)
         self.data_size = int(float(epoch_time)/0.001)
         # self.count = 0
-        # if file_name != ' ':
-        #     print('file use')
-        #     self.file = open(file_name+'.csv','w')
-        # else:
-        #     self.file = None
+        if file_name != ' ':
+            print('file use')
+            self.file = open(file_name+'.csv','w')
+        else:
+            self.file = None
 
     def __read(self,val=0):
         time.sleep(0.001)
-        # temp =  self.__adc.read_u16()
-        # temp *= conversion_factor
-        temp = random.random()
+        temp =  self.__adc.read_u16()
+        temp *= conversion_factor
+        # temp = random.random()
         # try:
         #     self.data[self.count] = temp
         #     self.count += 1
@@ -33,14 +33,14 @@ class emg_signal:
         #     self.data[self.count] = temp
         #     return readings
 
-        # if self.file != None:
-        #    self.file.write(str(temp)+",")
+        if self.file != None:
+           self.file.write(str(temp)+",")
         return temp
 
     def continuous_read(self,Ns=100,val=0):
         data = []
         for i  in range(self.data_size):
-            data.append(self.__read(val))
+            data.append(self.__read(val=val))
         return np.array(data)
 
 
