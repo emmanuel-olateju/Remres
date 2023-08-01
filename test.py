3b054289d3794320400ae177b162814c04b3abb6
MichaelAdesiyan <99549113+MichaelAdesiyan@users.noreply.github.com>
2023-08-01 05:29:11 +0100

    test

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ stacked_arrays = np.empty((0, 3))  # shape (0, 3) means 0 rows and 3 columns
 for i in range(3):
     arr = np.random.rand(1, 3)  # create a random 1x3 array
     stacked_arrays = np.vstack((stacked_arrays, arr))  # add the array to the stacked array
+    
 
 # Print the stacked array
 print(stacked_arrays)
