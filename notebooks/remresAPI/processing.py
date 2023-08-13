21a6f17d21524229b02f5ec999d7362f0d4a9d47
emmanuel-olateju <olatejuemmanuel@gmail.com>
2023-08-13 04:07:46 +0100

    wavelet computation started

--- notebooks/remresAPI/processing.py
+++ notebooks/remresAPI/processing.py
@@ -1,4 +1,9 @@
 from scipy.signal import stft
+from scipy.signal import cwt
+from scipy.signal import morlet
+from scipy.signal import ricker
+
+import numpy as np
 
 class preprocessor:
     def __init__(self,name):
@@ -16,3 +21,25 @@ class STFT(preprocessor):
 
     def fit_transform(self,X,y=None):
         return stft(X,self.__fs,nperseg=self.__ts)
+
+def wavelet(X,widths,function):
+    if X.ndim==2:
+        __wt = np.empty((0,widths.shape[0],X.shape[1]))
+        for i in range(X.shape[0]):
+            __wt = np.vstack((
+                __wt,
+                cwt(X[i,:],function,widths).reshape(1,widths.shape[0],X.shape[1])
+            ))
+        return __wt
+    elif X.ndim==1:
+        return cwt(X,function,widths)
+
+class Wavelet(preprocessor):
+
+    def __init__(self, name,widths,function=morlet):
+        super().__init__(name)
+        self.widths = widths
+        self.method = function
+    
+    def fit_transform(self,X,y=None):
+        return wavelet(X,self.widths,self.method)
